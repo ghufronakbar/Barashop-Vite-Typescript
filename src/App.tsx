dd70a0e64545b38a3dc5f11c127a50948c9ae548
ghufron akbar <alan.040219.gam@gmail.com>
2025-04-07 13:40:32 +0700

    Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
 import DetailProductPage from "./pages/DetailProductPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/product/:id" element={<DetailProductPage />} />
       </Routes>
--- src/pages/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage.tsx
@@ -4,13 +4,13 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const ResetPasswordPage = () => {
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
       <div className="border border-gray-300 px-4  py-4 w-[90%] md:w-2/3 lg:w-1/3 xl:w-1/4 rounded-xl drop-shadow-md flex flex-col gap-4 bg-white">
         <div>
-          <h1 className="text-2xl text-black font-medium">Selamat Datang</h1>
-          <p className="text-sm">Silahkan login untuk melanjutkan</p>
+          <h1 className="text-2xl text-black font-medium">Reset Password</h1>
+          <p className="text-sm">Masukkan email untuk ganti ulang password</p>
         </div>
         <div className="flex flex-col gap-2">
           <Label>Email</Label>
@@ -18,21 +18,17 @@ const LoginPage = () => {
             type="email"
             placeholder={`example@${APP_NAME?.toLowerCase()}.com`}
           />
-        </div>
-        <div className="flex flex-col gap-2">
-          <Label>Password</Label>
-          <Input type="password" placeholder="********" />
-        </div>
-        <Button className="mt-2">Login</Button>
+        </div>        
+        <Button className="mt-2">Reset</Button>
         <div className="flex flex-row items-center">
           <div className="w-full h-[1px] bg-gray-300" />
           <p className="px-2 text-xs flex-nowrap whitespace-nowrap text-center flex-1">
-            Lupa password?
+            Sudah punya akun?
           </p>
           <div className="w-full h-[1px] bg-gray-300" />
         </div>
-        <Link to={"/reset-password"} className="w-full">
-          <Button variant="outline" className="w-full">Reset Password</Button>
+        <Link to={"/login"} className="w-full">
+          <Button variant="outline" className="w-full">Login</Button>
         </Link>
         <span className="text-xs text-gray-400 text-center">
           &copy; {APP_NAME} 2025
@@ -42,4 +38,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default ResetPasswordPage;
