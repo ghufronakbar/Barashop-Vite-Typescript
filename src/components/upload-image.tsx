bdadb238eca4cd48b58436c4d52c932fa2f70795
ghufron akbar <alan.040219.gam@gmail.com>
2025-04-07 22:14:11 +0700

    CRUD Produk

---
+++ src/components/upload-image.tsx
@@ -0,0 +1,111 @@
+import { api } from "@/config/api";
+import { makeToast } from "@/helper/makeToast";
+import { cn } from "@/lib/utils";
+import { Api } from "@/model/Api";
+import React, { useState } from "react";
+
+interface Props {
+  className?: string;
+  image?: string | null;
+  onChangeImage?: (image: string | null) => void;
+}
+
+export const UploadImage: React.FC<Props> = ({
+  className,
+  image,
+  onChangeImage,
+}) => {
+  const [isDragOver, setIsDragOver] = useState(false);
+
+  const onImageSelected = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const file = e.target.files?.[0];
+      if (file) {
+        await handleFileUpload(file);
+      }
+    } catch (error) {
+      makeToast("error", error);
+    }
+  };
+
+  const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragOver(false);
+    const file = e.dataTransfer.files?.[0];
+    if (file) {
+      await handleFileUpload(file);
+    }
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragOver(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragOver(false);
+  };
+
+  const handleFileUpload = async (file: File) => {
+    try {
+      const formData = new FormData();
+      formData.append("image", file);
+      makeToast("info", "Uploading...");
+      const res = await api.post<Api<string>>("/image", formData);
+      onChangeImage?.(res.data.data);
+    } catch (error) {
+      makeToast("error", error);
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        "flex flex-col items-center justify-center gap-2 border border-dashed border-gray-300 rounded-md p-2 cursor-pointer hover:bg-gray-100 h-40 transition-all overflow-hidden",
+        className,
+        isDragOver ? "bg-gray-200" : ""
+      )}
+      onClick={() => document.getElementById("image")?.click()}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+    >
+      <input
+        className="hidden"
+        type="file"
+        accept="image/*"
+        id="image"
+        onChange={onImageSelected}
+      />
+      {image ? (
+        <img
+          src={image}
+          alt=""
+          className="w-full h-full object-cover rounded-md"
+          width={600}
+          height={400}
+        />
+      ) : (
+        <>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <p className="text-gray-500 text-sm">
+            Klik / Drag untuk mengunggah gambar
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
--- src/pages/master/ProdukPage.tsx
+++ src/pages/master/ProdukPage.tsx
@@ -18,11 +18,34 @@ import formatDate from "@/helper/formatDate";
 import { Input } from "@/components/ui/input";
 import { Plus, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
 import formatRupiah from "@/helper/formatRupiah";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { UploadImage } from "@/components/upload-image";
 
 const ProdukPage = () => {
-  const { filteredData, search, setSearch } = useProduks();
+  const {
+    filteredData,
+    search,
+    setSearch,
+    onClickItem,
+    setIsOpen,
+    form,
+    onChange,
+    isOpen,
+    onClickAdd,
+    setForm,
+    handleSubmit,
+    handleDelete,
+  } = useProduks();
   const TABLE_HEADERS = [
     "No",
     "",
@@ -38,12 +61,10 @@ const ProdukPage = () => {
     <DashboardLayout
       title="Produk"
       childredHeader={
-        <Link to={`/produk/tambah`}>
-          <Button variant="default">
-            <Plus />
-            Tambah
-          </Button>
-        </Link>
+        <Button variant="default" onClick={onClickAdd}>
+          <Plus />
+          Tambah
+        </Button>
       }
     >
       <div className="relative w-full md:w-fit min-w-[300px]">
@@ -86,10 +107,23 @@ const ProdukPage = () => {
                 <TableCell>{item.total_terjual}</TableCell>
                 <TableCell>{formatDate(item.updated_at, true, true)}</TableCell>
                 <TableCell className="flex flex-row gap-2">
-                  <Link to={`/produk/${item.id}`}>
-                    <Button variant="secondary">Edit</Button>
-                  </Link>
-                  <Button variant="destructive">Hapus</Button>
+                  <Button
+                    variant="secondary"
+                    onClick={() => onClickItem(item, true)}
+                  >
+                    Edit
+                  </Button>
+                  <AlertConfirmation
+                    trigger={
+                      <Button
+                        variant="destructive"
+                        onClick={() => onClickItem(item)}
+                      >
+                        Hapus
+                      </Button>
+                    }
+                    onConfirm={handleDelete}
+                  />
                 </TableCell>
               </TableRow>
             ))}
@@ -99,13 +133,124 @@ const ProdukPage = () => {
           </TableCaption>
         </Table>
       </div>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <DialogHeader>
+              <DialogTitle>
+                {form.id ? "Edit Produk" : "Tambah Produk"}
+              </DialogTitle>
+              <DialogDescription>
+                Isi form dibawah ini untuk{" "}
+                {form.id ? "mengedit" : "menambahkan"} produk
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-col gap-4">
+              <UploadImage
+                image={form.gambar}
+                onChangeImage={(val) => setForm({ ...form, gambar: val })}
+              />
+              <div className="flex flex-col gap-2">
+                <Label>Nama Produk</Label>
+                <Input
+                  placeholder="Bunga Matahari"
+                  value={form.nama}
+                  onChange={(e) => onChange(e, "nama")}
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Label>Kategori Produk</Label>
+                <Input
+                  placeholder="Bunga"
+                  value={form.kategori}
+                  onChange={(e) => onChange(e, "kategori")}
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Label>Harga Jual (Rp)</Label>
+                <Input
+                  placeholder="20000"
+                  value={form.harga}
+                  onChange={(e) => onChange(e, "harga")}
+                  type="number"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Label>Deskripsi</Label>
+                <Textarea
+                  value={form.deskripsi || ""}
+                  onChange={(e) => onChange(e, "deskripsi")}
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Simpan</Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
     </DashboardLayout>
   );
+};
+
+interface ProdukDTO {
+  id: string;
+  nama: string;
+  kategori: string;
+  harga: number;
+  deskripsi: string | null;
+  gambar: string | null;
+}
+
+const initProdukDTO: ProdukDTO = {
+  id: "",
+  nama: "",
+  kategori: "",
+  harga: 0,
+  deskripsi: null,
+  gambar: null,
 };
 
 const useProduks = () => {
   const [data, setData] = useState<Produk[]>([]);
   const [search, setSearch] = useState("");
+  const [form, setForm] = useState<ProdukDTO>(initProdukDTO);
+  const [isOpen, setIsOpen] = useState(false);
+  const [pending, setPending] = useState(false);
+
+  const onClickItem = (item: Produk, isEdit?: boolean) => {
+    setForm({
+      id: item.id,
+      harga: item.harga,
+      kategori: item.kategori,
+      nama: item.nama,
+      deskripsi: item.deskripsi,
+      gambar: item.gambar,
+    });
+    if (isEdit) setIsOpen(true);
+  };
+
+  const onClickAdd = () => {
+    setForm(initProdukDTO);
+    setIsOpen(true);
+  };
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    key: keyof ProdukDTO
+  ) => {
+    setForm({
+      ...form,
+      [key]: e.target.value,
+    });
+  };
+
   const filteredData = data.filter((item) =>
     item.nama.toLowerCase().includes(search.toLowerCase())
   );
@@ -123,7 +268,109 @@ const useProduks = () => {
     fetchData();
   }, []);
 
-  return { filteredData, search, setSearch };
+  const handleSubmit = async () => {
+    try {
+      Object.entries(form).forEach(([key, value]) => {
+        if (!value && key !== "id" && key !== "gambar" && key !== "deskripsi") {
+          throw new Error("Semua kolom harus diisi");
+        }
+      });
+      if (pending) return;
+      setPending(true);
+      makeToast("info");
+      if (form.id) {
+        await api.put(`/produk/${form.id}`, form);
+        await fetchData();
+        makeToast("success", "Berhasil mengedit produk");
+      } else {
+        await api.post("/produk", form);
+        await fetchData();
+        makeToast("success", "Berhasil menambahkan produk");
+      }
+      setIsOpen(false);
+      setForm(initProdukDTO);
+    } catch (error) {
+      makeToast("error", error);
+    } finally {
+      setPending(false);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      if (!form.id) return;
+      if (pending) return;
+      setPending(true);
+      makeToast("info");
+      await api.delete(`/produk/${form.id}`);
+      await fetchData();
+      makeToast("success", "Berhasil menghapus produk");
+    } catch (error) {
+      makeToast("error", error);
+    } finally {
+      setPending(false);
+    }
+  };
+
+  return {
+    filteredData,
+    search,
+    setSearch,
+    onClickItem,
+    setIsOpen,
+    form,
+    onChange,
+    isOpen,
+    onClickAdd,
+    setForm,
+    handleSubmit,
+    handleDelete,
+  };
 };
 
 export default ProdukPage;
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+interface Props {
+  trigger: React.ReactNode;
+  message?: string;
+  onConfirm?: () => void;
+}
+
+const AlertConfirmation: React.FC<Props> = ({
+  trigger,
+  message,
+  onConfirm,
+}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {message || "Apakah kamu yakin ingin menghapus?"}
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Tindakan ini tidak dapat dibatalkan. Ini akan menghapus akun Anda
+            secara permanen dan menghapus data Anda secara permanen.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Tutup</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Ya</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
