52541faef2083e0a6a2f3c96972e7233f9ac759d
ghufron akbar <alan.040219.gam@gmail.com>
2025-04-08 16:20:23 +0700

    Delete unused

--- src/pages/DetailProductPage.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const DetailProductPage = () => {
-  const { id } = useParams();
-  return <div>DetailProduct {id}</div>;
-};
-
-export default DetailProductPage;
